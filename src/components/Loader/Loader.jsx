49b9b41d6029872750d880a06fec5591b5f15c37
vas24757 <vas24757@uvg.edu.gt>
2024-05-08 10:49:52 -0600

    loader v2

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,12 +5,16 @@ function Loader() {
 
   return (
     <div className="flex-col gap-4 w-full flex items-center justify-center">
-      <div className="w-28 h-28 border-8 text-blue-400 text-4xl animate-spin border-gray-300 flex items-center justify-center border-t-blue-400 rounded-full">
+      
+      <div className="w-28 h-28 absolute z-10">
         <img
           src="/src/assets/images/Logo.png"
           alt="Logo de delfín"
           className="w-full h-full object-cover rounded-full"
         />
+      </div>
+      <div className="w-28 h-28 border-8 text-blue-400 text-4xl border-gray-300 flex items-center justify-center border-t-blue-400 rounded-full animate-spin z-0">
+       
       </div>
     </div>
   );
